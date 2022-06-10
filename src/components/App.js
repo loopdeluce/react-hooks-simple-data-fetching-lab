0a194c41c912ba461ffe9c46f43bf2f04a2c1262
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-06-10 11:57:46 -0600

    commit tree 6f724f4fcf984d853c5448a9edb57b2bfd931ce4

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [image, setImage] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(resp => resp.json())
+    .then(image => {
+      setImage(image.message);
+      setIsLoading(false)
+    }
+      )
+  }, [])
+
+  if (isLoading === true) {
+    return <p>Loading...</p>;
+  };
+
+  return (
+    <img src={image} alt="A Random Dog" />
+  );
+};
+
+export default App;
